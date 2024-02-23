45eb343f740d2e908664e81d1dff807db95273c8
Dmitry Pilyuk <dmitriipiliuk@gmail.com>
2024-02-23 17:10:08 +0300

    Fix pattern Effect eith args

--- OCamlWithEffects/lib/parser/parser.ml
+++ OCamlWithEffects/lib/parser/parser.ml
@@ -153,13 +153,15 @@ let parse_pattern =
   let parse_pattern_list_constr =
     chainr1 (parens self <|> parse_primitive_pattern) parse_pattern_list_constr
   in
-  choice
-    [ parse_pattern_list_constr
-    ; parse_pattern_with_args self
-    ; parse_primitive_pattern
-    ; parse_pattern_tuple self
-    ; parens self
-    ]
+  let p_parser =
+    choice
+      [ parse_pattern_list_constr
+      ; parse_pattern_with_args self
+      ; parse_primitive_pattern
+      ; parse_pattern_tuple self
+      ]
+  in
+  parse_pattern_with_args p_parser <|> p_parser
 ;;
 
 (* ---------------- *)
