942ecc5e2ae6f6f22775e43e01cb65358af1806c
raf-nr <nurmuhametovrafik04@gmail.com>
2024-02-27 01:39:51 +0300

    Add parser tests

--- OCamlWithEffects/tests/parser/parse_pattern_matching.ml
+++ OCamlWithEffects/tests/parser/parse_pattern_matching.ml
@@ -5,9 +5,11 @@
 open Ocaml_with_effects_lib.Run
 
 let%expect_test _ =
-parse_with_print {| let f x = match x with 
-  | 0 -> "zero"
-  | _ -> "not zero"
+parse_with_print {|
+  let f x = 
+    match x with 
+    | 0 -> "zero"
+    | _ -> "not zero"
   |};
   [%expect
     {|
@@ -16,6 +18,33 @@ parse_with_print {| let f x = match x with
            (EMatchWith ((EIdentifier "x"),
               [((PConst (Int 0)), (EConst (String "zero")));
                 (PAny, (EConst (String "not zero")))]
+              ))
+           )),
+        None))
+      ] |}]
+;;
+
+let%expect_test _ =
+parse_with_print {|
+  let f x = 
+    (match x with 
+    | [] -> 0
+    | hd :: tl -> 
+      (match hd with
+      | x -> x
+      | _ -> hd))
+  |};
+  [%expect
+    {|
+    [(EDeclaration ("f",
+        (EFun ((PVal "x"),
+           (EMatchWith ((EIdentifier "x"),
+              [(PNill, (EConst (Int 0)));
+                ((PListCons ((PVal "hd"), (PVal "tl"))),
+                 (EMatchWith ((EIdentifier "hd"),
+                    [((PVal "x"), (EIdentifier "x")); (PAny, (EIdentifier "hd"))]
+                    )))
+                ]
               ))
            )),
         None))
