be4386331464137b1250bc0cedd70b338797687f
raf-nr <nurmuhametovrafik04@gmail.com>
2024-02-27 03:38:11 +0300

    Add interpreter tests for bynary operations

---
+++ OCamlWithEffects/tests/interpreter/int_simple_expr.ml
@@ -0,0 +1,79 @@
+(** Copyright 2021-2024, DmitryPilyuk and raf-nr *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open Ocaml_with_effects_lib.Run
+
+(* Bynary operations *)
+
+let%expect_test _ =
+  interpret {| 5 + 6 |};
+  [%expect {|
+    - : int = 11 |}]
+;;
+
+let%expect_test _ =
+  interpret {| 5 - 6 / 3 * 2 |};
+  [%expect {|
+    - : int = 4 |}]
+;;
+
+let%expect_test _ =
+  interpret {| 4 / 0 |};
+  [%expect {|
+    Error: division by zero. |}]
+;;
+
+let%expect_test _ =
+  interpret {|
+    let res1 = true && true
+    let res2 = true && false
+    let res3 = false && false
+  |};
+  [%expect {|
+    val res1 : bool = true
+    val res2 : bool = false
+    val res3 : bool = false |}]
+;;
+
+let%expect_test _ =
+  interpret {|
+    let res1 = true || true
+    let res2 = true || false
+    let res3 = false || false
+  |};
+  [%expect {|
+    val res1 : bool = true
+    val res2 : bool = true
+    val res3 : bool = false |}]
+;;
+
+let%expect_test _ =
+  interpret {| (1 + 1 <= 1 + 1) && (1 + 1 >= 1 + 1) |};
+  [%expect {|
+    - : bool = true |}]
+;;
+
+let%expect_test _ =
+  interpret {| (1 + 1 < 1 + 1 + 1) && (1 + 1 + 1 > 1 + 1) && (1 + 1 + 1 != 1 + 1) && (1 + 1 <> 1 + 1 + 1)|};
+  [%expect {|
+  - : bool = true |}]
+;;
+
+let%expect_test _ =
+  interpret {| 'a' < 'b' && 'b' > 'a' |};
+  [%expect {|
+    - : bool = true |}]
+;;
+
+let%expect_test _ =
+  interpret {| "name1" < "name2" && "name2" > "name1" |};
+  [%expect {|
+    - : bool = true |}]
+;;
+
+let%expect_test _ =
+  interpret {| () = () && not (() != ())|};
+  [%expect {|
+    - : bool = true |}]
+;;
